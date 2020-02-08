9ea0a1f56d853bf4f2c43a85725b92c46fa24cc9
Sylvain Pascou <sylvain@pascou.net>
2020-02-08 17:33:55 +0900

    Rebalance level costs

--- src/main/java/net/liquidwarpmc/strongandfairanvils/mixin/AnvilContainerMixin.java
+++ src/main/java/net/liquidwarpmc/strongandfairanvils/mixin/AnvilContainerMixin.java
@@ -7,7 +7,10 @@ import net.minecraft.block.BlockState;
 import net.minecraft.container.AnvilContainer;
 import net.minecraft.container.BlockContext;
 import net.minecraft.container.Property;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -38,6 +41,13 @@ public abstract class AnvilContainerMixin {
     @Final
     private Property levelCost;
 
+    @Shadow
+    @Final
+    private Inventory inventory;
+
+    /**
+     * Disable the default prior work penalty increase.
+     */
     @Redirect(
             method = "updateResult",
             at = @At(
@@ -47,9 +57,15 @@ public abstract class AnvilContainerMixin {
             ),
             require = 1
     )
-    private int preventPriorWorkPenaltyIncrease(int cost) { return cost; }
+    private int preventDefaultPriorWorkPenaltyIncrease(int cost) {
+//        StrongAndFairAnvils.LOGGER.info("Preventing default prior work penalty increase");
+        return cost;
+    }
 
 
+    /**
+     * Don't increase level cost for repair between unenchanted items.
+     */
     @ModifyVariable(
             method = "updateResult",
             at = @At(
@@ -60,8 +76,22 @@ public abstract class AnvilContainerMixin {
             ordinal = 0,
             require = 1
     )
-    private int freeLevelRepairCostWithItem(int val) { return val - 1; }
+    private int freeLevelRepairCostBetweenUnenchantedItem(int val) {
+        ItemStack itemStack = this.inventory.getInvStack(0);
+        ItemStack itemStack3 = this.inventory.getInvStack(1);
 
+        if(itemStack.hasEnchantments() || itemStack3.hasEnchantments()) {
+//            StrongAndFairAnvils.LOGGER.info("Enchanted items: costly repair");
+            return val;
+        } else {
+//            StrongAndFairAnvils.LOGGER.info("Unenchanted items: free repair");
+            return val - 1;
+        }
+    }
+
+    /**
+     * Don't increase level cost for repair with materials for unenchanted item.
+     */
     @ModifyVariable(
             method = "updateResult",
             at = @At(
@@ -72,8 +102,21 @@ public abstract class AnvilContainerMixin {
             ordinal = 0,
             require = 1
     )
-    private int freeLevelRepairCostWithMaterial(int val) { return val - 2; }
+    private int freeLevelRepairCostWithMaterialOfUnenchantedItem(int val) {
+        ItemStack itemStack = this.inventory.getInvStack(0);
 
+        if(itemStack.hasEnchantments()) {
+//            StrongAndFairAnvils.LOGGER.info("Enchanted item: costly repair with material");
+            return val;
+        } else {
+//            StrongAndFairAnvils.LOGGER.info("Unenchanted item: free repair with material");
+            return val - 2;
+        }
+   }
+
+    /**
+     * Don't increase level cost for name operations.
+     */
     @ModifyVariable(
             method = "updateResult" ,
             at = @At(
@@ -86,9 +129,15 @@ public abstract class AnvilContainerMixin {
             ordinal = 0,
             require = 1
     )
-    private int freeNameResetCost(int val) { return val - 1; }
+    private int freeNameResetCost(int val) {
+//        StrongAndFairAnvils.LOGGER.info("Free name reset");
+        return val - 1;
+    }
 
 
+    /**
+     * Don't increase level cost for name operations.
+     */
     @ModifyVariable(
             method = "updateResult" ,
             at = @At(
@@ -101,8 +150,14 @@ public abstract class AnvilContainerMixin {
             ordinal = 0,
             require = 1
     )
-    private int freeNameChangeCost(int val) { return val - 1; }
+    private int freeNameChangeCost(int val) {
+//        StrongAndFairAnvils.LOGGER.info("Free name change");
+        return val - 1;
+    }
 
+    /**
+     * Allow operations without level cost.
+     */
     @ModifyConstant(
             method = "updateResult",
             constant = @Constant(expandZeroConditions = Constant.Condition.GREATER_THAN_ZERO, ordinal = 0),
@@ -113,12 +168,15 @@ public abstract class AnvilContainerMixin {
     )
     private int allowZeroLevelCost(int value) { return -1; }
 
+    /**
+     */
     @Inject(
             method = "updateResult",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/container/Property;set(I)V",
-                    ordinal = 0
+                    ordinal = 0,
+                    shift = At.Shift.AFTER
             ),
             slice = @Slice(
                     from = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setCustomName(Lnet/minecraft/text/Text;)Lnet/minecraft/item/ItemStack;", ordinal = 0)
@@ -126,20 +184,34 @@ public abstract class AnvilContainerMixin {
             locals = LocalCapture.CAPTURE_FAILHARD,
             require = 1
     )
-    private void emptyNoChangesResult(CallbackInfo ci,  ItemStack itemStack, int i, int j, int k, ItemStack itemStack2, ItemStack itemStack3, Map map) {
+    private void finalManipulation(CallbackInfo ci,  ItemStack itemStack, int i, int j, int k, ItemStack itemStack2, ItemStack itemStack3, Map map) {
         // itemStack if the first slot (original item)
         // itemStack3 is the second slot (second item, material, enchanted book, etc)
         // itemStack2 is the result
 
-        if(
-                itemStack3 == ItemStack.EMPTY
-                        && ItemStack.areItemsEqual(itemStack, itemStack2)
-                        && ItemStack.areTagsEqual(itemStack, itemStack2)
-        ) {
-            ((SetEmptyItemStackInterface)(Object) itemStack2).strongandfairanvils_setEmpty();
+        if(itemStack3 == ItemStack.EMPTY && ItemStack.areItemsEqual(itemStack, itemStack2)) {
+            if(ItemStack.areTagsEqual(itemStack, itemStack2)) {
+                // Clear result slot if no changes to the item are made.
+//                StrongAndFairAnvils.LOGGER.info("No change");
+                ((SetEmptyItemStackInterface) (Object) itemStack2).strongandfairanvils_setEmpty();
+            } else {
+                // Set the cost to 0 if no other changes than name have been done.
+//                StrongAndFairAnvils.LOGGER.info("Name change only");
+                this.levelCost.set(0);
+            }
+        } else {
+            // Increase prior work penalty if item enchantments have been manipulated.
+            Map<Enchantment, Integer> map_original = EnchantmentHelper.getEnchantments(itemStack);
+            if(!map.equals(map_original)) {
+//                StrongAndFairAnvils.LOGGER.info("Enchantments manipulated: increasing prior work penalty");
+                itemStack2.setRepairCost(AnvilContainer.getNextCost(itemStack2.getRepairCost()));
+            }
         }
     }
 
+    /**
+     * Only allow costless operations to be performed on the stone anvil.
+     */
     @Inject(
             method = "updateResult",
             at = @At(
@@ -155,7 +227,7 @@ public abstract class AnvilContainerMixin {
             locals = LocalCapture.CAPTURE_FAILHARD,
             require = 1
     )
-    private void stoneAnvilNoLevelUseOperations(CallbackInfo ci,  ItemStack itemStack, int i, int j, int k, ItemStack itemStack2, ItemStack itemStack3, Map map) {
+    private void stoneAnvilNoLevelUseOperationsOnly(CallbackInfo ci,  ItemStack itemStack, int i, int j, int k, ItemStack itemStack2, ItemStack itemStack3, Map map) {
         boolean isStoneAnvil = this.context.run((world, blockPos) -> {
             BlockState blockState = world.getBlockState(blockPos);
             Block block = blockState.getBlock();
@@ -168,6 +240,9 @@ public abstract class AnvilContainerMixin {
         }
     }
 
+    /**
+     * Whitelist the stone anvil for AnvilInventory screen to show properly.
+     */
     @Inject(
             method = "method_17366",
             at = @At("RETURN"),
